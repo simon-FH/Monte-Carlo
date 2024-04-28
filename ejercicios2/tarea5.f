91f9fd98b25b8675110a7e8a4d90574aff33d76b
simon <s.fonseca03@ufromail.cl>
2024-04-28 00:01:31 -0400

    tarea 5

--- ejercicios2/tarea5.f
+++ ejercicios2/tarea5.f
@@ -1,22 +1,20 @@
         program congruencias
-            integer,parameter :: seed = 86456127
+            integer :: A = 16843009
+            integer :: B = 301489861
+            integer :: N = 2**32
+            integer :: r = 0
 
-            call srand(seed)
+            open(unit=11,file='pseudoCongr10.csv',status='unknown')
+            do i = 1, 10
+                r = mod(A*r + B,N)
+                write(11,*) r
+            enddo
 
-            open(unit=11,file='data_out_4_mil.csv',status='unknown')
+            open(unit=12,file='pseudoCongr1000.csv',status='unknown')
             do i = 1, 1000
-                rad = sqrt(rand())
-                ang = 2*3.14159*rand()
-                write(11,*) rad*cos(ang)-1, ",", rad*sin(ang)+2
+                r = mod(A*r + B,N)
+                write(12,*) r
             enddo
-            close(11)
-            
-            open(unit=12,file='data_out_4_millon.csv',status='unknown')
-            do i = 1, 1000000
-                rad = sqrt(rand())
-                ang = 2*3.14159*rand()
-                write(12,*) rad*cos(ang)-1, ",", rad*sin(ang)+2
-            enddo
-            close(12)
+
 
         end program congruencias
